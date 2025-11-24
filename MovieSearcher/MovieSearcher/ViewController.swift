a2bb81626e0c0dd3f5dbc519f55d0032b29a7e41
Joey <jiasin.liu@sinopac.com>
2025-11-24 09:29:50 +0800

    [feat] initial commit for creating xcode project

---
+++ MovieSearcher/MovieSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearcher
+//
+//  Created by JiaSin Liu on 2025/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieSearcher/MovieSearcherTests/MovieSearcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearcherTests.swift
+//  MovieSearcherTests
+//
+//  Created by JiaSin Liu on 2025/11/24.
+//
+
+import Testing
+@testable import MovieSearcher
+
+struct MovieSearcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
